98992f867ec122636ae63d7e57dd838fd03629a3
DAW <daw@daw-VirtualBox.(none)>
2014-03-19 19:27:45 +0100

    ejercicio jQueryPlugin

--- jQueryPlugin/js/validatorplugin.js
+++ jQueryPlugin/js/validatorplugin.js
@@ -1,16 +1,33 @@
 //***********Este es nuestro plugin jQuery que valida un formulario**************************************
 (function($){
-    $.fn.validate = function(opts){
-        //El this es cada una de las tablas del DOM
+    var opts;
+     $form   = $(this);
+        //Usamos los selectore se jQuery en vez de los de js
+        //inputs = form.querySelectorAll('input[data-validator], textarea[data-validator], select[data-validator]'),
+        //submit = form.querySelector('input[type="submit"]');
+        //es una buena práctica poner el $ a las vbles. cuando son de jquery
+        $inputs = $('input[data-validator], textarea[data-validator], select[data-validator]');
+        $submit = $('input[type="submit"]');
+        var errors = [];
+    //Para crear el plugin tenemos que extender el prototipo de jQuery
+    $.fn.validate = function(options){
+        //El this es cada formulario
+        //para cada formulario hay que ejecutar solo esto:
+        opts = $.extend($.fn.validate.defaults, opts);
+        //filtramos por formularios
         return this.filter('form').each(function(){
+            //Anado los listeners que correspondan a los elmtos. según su tipo
+            //Escuchar el submit, nunca el botón. Puede no haber botón
+            $form.on('submit', validateForm);
+            $form.on('blur validate', 'input[data-validator]', validateField);
             var $this = $(this);
-            var errors = [],
-        form   = this,
-        inputs = form.querySelectorAll('input[data-validator], textarea[data-validator], select[data-validator]'),
-        submit = form.querySelector('input[type="submit"]');
 
-    var DEFAULT_OPTS = {
-        'required' : {
+        } );
+    };//Fin validate
+//Le añado una nueva propiedad a la función que acabo de crear. Porque LA FUNCION ES UN OBJETO
+//Se hace así para que esto sea público y pueda ser modificado
+ $.fn.validate.defaults = {
+      'required' : {
             'error' : 'This field should not be blank.',
             'class' : 'error'
         },
@@ -28,58 +45,63 @@
         }
     };
 
-    opts = mergeRecursive(DEFAULT_OPTS, opts);
-
     var disableSubmit = function() {
-        if(submit) {
-            submit.value = "Validating...";
-            submit.disabled = true;
+        if($submit) {
+            $submit.value = "Validating...";
+            $submit.disabled = true;
         }
     };
 
     var enableSubmit = function() {
-        if(submit) {
-            submit.value = "Send";
-            submit.disabled = false;
+        if($submit) {
+            $submit.value = "Send";
+            $submit.disabled = false;
         }
     };
-
+    //e es el evento que la lanza
+    //El this aquí haría ref. a un objeto del DOM
     var validateField = function(e) {
         if(APP.validator[this.dataset.validator]) {
             if(!APP.validator[this.dataset.validator](this.value)) {
                 console.log(this, opts[this.dataset.validator].error);
                 errors.push([this, opts[this.dataset.validator].error]);
+                //si está mal lo pongo en rojo
+                $(this).css('background-color', '#FF0000');
+            }else{
+                $(this).css('background-color', '#FFFFFF');
             }
         }
     };
 
     var validateForm = function(e) {
-        var ev = new Event('validate');
-
+       // var ev = new Event('validate');
         errors = [];
         disableSubmit();
-
-        for (var i = inputs.length - 1; i >= 0; i--) {
+        for (var i = $inputs.length - 1; i >= 0; i--) {
             //Lanzo los eventos en cada elemento input
-            inputs[i].dispatchEvent(ev);
+            //en jQuery el equivalente a dispatchEvent es trigger
+            //$inputs[i].dispatchEvent(ev);
+            $($inputs[i]).trigger('validate');
         }
-
         if(errors.length > 0) {
             e.preventDefault();
-
             enableSubmit();
-            console.log(errors);
+            console.log('errors: '+errors);
         }
     };
 
-    form.addEventListener('submit', validateForm);
-    for (var i = inputs.length - 1; i >= 0; i--) {
+    //form.addEventListener('submit', validateForm);
+    //$form.on('submit', validateForm);
+    //for (var i = $inputs.length - 1; i >= 0; i--) {
         //Añado los listeners
-        inputs[i].addEventListener('blur', validateField);
-        inputs[i].addEventListener('validate', validateField);
-    }
+        //En jQuery usar .on en vez de .addEventListener
+        //inputs[i].addEventListener('blur', validateField);
+        //inputs[i].addEventListener('validate', validateField);
+      //  $($inputs[i]).on('blur', validateField);
+      //  $($inputs[i]).on('validate', validateField);
+    //}
 
-    function mergeRecursive(dst, src) {
+    /*function mergeRecursive(dst, src) {
         for (var p in src) {
             try {
                 // Property in destination object set; update its value.
@@ -99,12 +121,10 @@
 
         return dst;
     }
+*/
 
 
 
-        });
-    };
-})(jQuery);
 
 
 
@@ -176,3 +196,4 @@ APP.validator = (function(){
     };
 })();
 
+})(jQuery);

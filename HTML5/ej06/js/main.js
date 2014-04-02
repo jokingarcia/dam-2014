85a300078684cc055a9079d50e4a11dfd8e08683
jokingarcia <jokingarcia75@hotmail.com>
2014-04-03 00:03:44 +0200

    ej06 de HTML5: reproductor de video

---
+++ HTML5/ej06/js/main.js
@@ -0,0 +1,62 @@
+//Ejercicio 3 HTML 5. Formulario
+$(document).ready(function(){
+    "use strict";
+    var video = $('#firstvideo');
+   //Reproducir los vídeos independientemente del codec soportado por el navegador.
+  //Incluir controles de reproducción, pausa, parar, avanzar y retroceder 10  segundos, inicio y fin.
+  // Control de volumen y paso a pantalla completa.
+  // Un indicador de progreso de la reproducción.
+   $(document).on('click','#play',function(e){
+        console.log("play");
+         video[0].play();
+  });
+    $(document).on('click','#pause',function(e){
+        console.log("pause");
+         video[0].pause();
+  });
+  $(document).on('click','#stop',function(e){
+        console.log("stop");
+        //OJO: hay que poner [0] aunque solo haya uno
+        video[0].currentTime=0;
+        video[0].pause();
+  });
+  $(document).on('click','#forward',function(e){
+        console.log("forward");
+         video[0].currentTime = video[0].currentTime + 10;
+  });
+  $(document).on('click','#backward',function(e){
+        console.log("backward");
+        video[0].currentTime = video[0].currentTime - 10;
+  });
+  $(document).on('click','#start',function(e){
+        console.log("start");
+        video[0].currentTime = 0;
+  });
+  $(document).on('click','#end',function(e){
+        console.log("end");
+        video[0].currentTime = video[0].duration;
+  });
+  $(document).on('click','#fullscreen',function(e){
+        console.log("fullscreen");
+       //TODO: aquí habría que  hacerlo para diferentes navegadores
+        video[0].webkitEnterFullScreen();
+  });
+  $(document).on('click','#volume',function(e){
+        console.log("volume");
+        video[0].volume = $('#volume')[0].value/100;
+  });
+  function setupSeekbar(){
+    seekbar.min = video.startTime;
+    seekbar.max = video.duration;
+  }
+  //TODO: aquí hay que hacer video.ontimeupdate para que vaya actualizando el progress
+  video.addEventListener('timeupdate', function (e){
+    $progreso.val(this.currentTime);
+  });
+  $(document).on('click','#progress',function(e){
+        console.log("progress");
+        $('#progress').value=video[0].currentTime;
+  });
+  //TODO: Se podría usar el modernizr y cargar en el source el video correspondiente
+
+});
